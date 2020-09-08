aee1f6ad8ff14163ba3c67a202c9cd927e544a29
kauttoj <janne.kauttonen@gmail.com>
2020-09-08 13:35:48 +0300

    added polygon region mapping option with a small GUI tool to create regions

--- IsochroneMapper.py
+++ IsochroneMapper.py
@@ -11,7 +11,11 @@ and use URL http://localhost:9080/otp/routers/hsl/index/graphql
 
 Either way, polling is slow with speed around ~1.3/sec
 
-16.2.2020 Janne Kauttonen
+-Janne Kauttonen
+
+version history
+16.2.2020 first version
+30.8.2020 added polygon mapping with separate GUI tool
 '''
 
 import numpy as np
@@ -26,10 +30,13 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import matplotlib.cm as plt_cm
 import random
+import pandas
 import branca
 from scipy.interpolate import griddata
 import geojsoncontour
+from shapely.geometry import Polygon, Point
 import math
+from scipy.ndimage.filters import gaussian_filter
 
 ## PARAMETERS - change these according to your taste and API service ##############################################
 API_URL = r"https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql" # internet
@@ -39,49 +46,53 @@ MAX_REQUEST_PER_SEC = 500 # careful not to get your IP banned for too fast reque
 # waltti regions	https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql
 # Entire Finland	https://api.digitransit.fi/routing/v1/routers/finland/index/graphql
 
-RESULTFILE = "DistanceMapper_results.pickle" # save results
-TIME_RANGES = [10,20,30,40,50,60] # in minutes, routes beyond these are not plotted with contours
+RESULTFILE = "IsochroneMapper_results.pickle" # save results
+TIME_RANGES = [20,30,40,50,60,70,80] # in minutes, routes beyond these are not plotted with contours
 # MAPPING_AREA = {"TYPE":"RECT","CORNERS":(
 #     (60.283088, 24.489482), # left top
 #     (60.100719, 25.195741), # right bottom
 # )} # X increases towards left, Y increases towards down
-MAPPING_AREA = {"TYPE":"CIRCLE","DISTANCE": 30} # distance in km
-MAX_POINTS = 30000 # maximum number of polling points (randomized), Note: 20k takes few hours
+# MAPPING_AREA = {"TYPE":"CIRCLE","DISTANCE": 30} # distance in km
+MAPPING_AREA = {"TYPE":"POLYGON","FILE": "HKI_polygon_coordinates.txt"} # distance in km
+MAX_POINTS = 40000 # maximum number of polling points (randomized), Note: 20k takes few hours
 OPACITY = 0.5 # layer opacity value for contours
-MAPPING_ACCURACY_MIN = 0.250 # km, rounded down (smaller = more grid points)
+MAPPING_ACCURACY_MIN = 0.200 # km, rounded down (smaller = more grid points)
 TARGET_COORDINATE = (60.201672, 24.934188) # poll distance from here (e.g., your home or work)
 # transport parameters for polling, defined by API service
 TRANSPORT_PARAMETERS = {
-    "date": "2020-02-20",
-    "time": "08:40:00",
+    "date": "2020-09-09",
+    "time": "08:00:00",
     "numItineraries": 3,
     "transportModes": ["RAIL","TRAM","WALK","BUS","SUBWAY"], # {"BUS","RAIL":True,"TRAM":True,"FERRY":False,"WALK":True,"SUBWAY":True}
     "walkReluctance": 1.0, # walk duration multiplier
-    "walkBoardCost": 1*60, # sec
-    "minTransferTime": 4*60, # sec
-    "walkSpeed": 1.5, # 1.33 m/s
+    "walkBoardCost": 2*60, # sec
+    "minTransferTime": 2*60, # sec
+    "walkSpeed": 1.60, # 1.33 m/s
     "maxWalkDistance": 2000, # m
     "maxTransfers": 1,
     "ignoreRealtimeUpdates":True,
 }
+USE_OLD_DATA = False
+isLegalPoint = lambda x,y: True  # glocal function handle (to be updated later)
 
 # NOTE: below is an optional area-limiting function that you can use to limit valid points and save some time in polling.
 # For most simple case, can be a separating line as below
-def is_legal_point(x):
+def line_constraint(x):
     x1,y1=60.186486, 25.330735
     x2,y2=60.092571, 24.670135
     if x[1] < y2 + ((y2-y1)/(x2-x1))*(x[0]-x2):
         return True
     return False
 
-#def is_legal_point(x):
-#    return True # no criteria, all points in region valid
-
 #############################################################################################################
 
 # conversion to km
 def degree_to_km(x,y):
-    return 110.574,111.320*math.cos(math.radians(x))
+    return [110.574,111.320*math.cos(math.radians(x))]
+
+def km_to_degree(x,y):
+    xx,yy = degree_to_km(TARGET_COORDINATE[0],TARGET_COORDINATE[1])
+    return [x/xx,y/yy]
 
 # get number of steps for a rectangle
 def get_stepcount(rect):
@@ -93,26 +104,55 @@ def get_stepcount(rect):
     y_step_dist = (rect[3][1]-rect[0][1])/y_dist_step
     return x_step_dist,x_dist_step,y_step_dist,y_dist_step
 
+# sample points within polygon
+def polygon_points(mypoints):
+    global isLegalPoint
+    poly = Polygon(mypoints)
+    def random_points_within(poly, num_points):
+        min_x, min_y, max_x, max_y = poly.bounds
+        points = []
+        while len(points) < num_points:
+            random_point = Point([random.uniform(min_x, max_x), random.uniform(min_y, max_y)])
+            if (random_point.within(poly)):
+                points.append((random_point.coords.xy[0][0],random_point.coords.xy[1][0]))
+        return points
+    points = random_points_within(poly, MAX_POINTS)
+    isLegalPoint = lambda x,y:Point([x,y]).within(poly) # update handle
+    return points
+
 # compute points
 def compute_coordinates(mapping_dict):
-    if mapping_dict["TYPE"]=="CIRCLE":
+    global isLegalPoint
+    np.random.seed(1)
+    random.seed(1)
+    points, distance_from_target = [], []
+    if mapping_dict["TYPE"]=="POLYGON":
+        if "FILE" in mapping_dict:
+            coords = pandas.read_csv(mapping_dict["FILE"],names=["LAT","LON"]).values.tolist()
+        else:
+            coords = mapping_dict["POINTS"]
+        points = polygon_points(coords)
+        for p in points:
+            distance_from_target.append(geopy.distance.geodesic(p, TARGET_COORDINATE).km)
+        return points, distance_from_target
+    elif mapping_dict["TYPE"]=="CIRCLE":
         dtokm = degree_to_km(TARGET_COORDINATE[0],TARGET_COORDINATE[1])
         rect_corners=(
             (TARGET_COORDINATE[0]+mapping_dict["DISTANCE"]/dtokm[0],TARGET_COORDINATE[1]-mapping_dict["DISTANCE"]/dtokm[1]),
             (TARGET_COORDINATE[0]-mapping_dict["DISTANCE"]/dtokm[0],TARGET_COORDINATE[1]+mapping_dict["DISTANCE"]/dtokm[1])
         )
-    else:
+    elif mapping_dict["TYPE"]=="RECT":
         rect_corners=mapping_dict["CORNERS"]
+
     rect = (rect_corners[0],
             (rect_corners[1][0],rect_corners[0][1]),
             rect_corners[1],
             (rect_corners[0][0],rect_corners[1][1]),)
     x_step_dist, x_dist_step, y_step_dist, y_dist_step = get_stepcount(rect)
-    points,distance_from_target = [],[]
     for x in range(x_dist_step):
         for y in range(y_dist_step):
             new_point = (rect[1][0] + x * x_step_dist,rect[0][1] + y * y_step_dist)
-            if is_legal_point(new_point):
+            if line_constraint(new_point):
                 if mapping_dict["TYPE"]=="CIRCLE":
                     dist = geopy.distance.geodesic(new_point,TARGET_COORDINATE).km
                     if dist > MAPPING_AREA["DISTANCE"]:
@@ -260,7 +300,7 @@ def get_colormap(z_orig):
     return cm,color_list,TIME_RANGES_dense
 
 # add contour to map
-def add_contour(x_orig,y_orig,z_orig,my_map_global,cm,layer_name,threshold=None,add_colormap=True,show = True):
+def add_contour(x_orig,y_orig,z_orig,my_map_global,cm,layer_name,threshold=None,add_colormap=True,show = True,smooth_sigma_km=0):
 
     # The original data
     x_orig = np.array(x_orig)
@@ -279,15 +319,31 @@ def add_contour(x_orig,y_orig,z_orig,my_map_global,cm,layer_name,threshold=None,
     y_arr = np.linspace(np.min(x_orig), np.max(x_orig), max(200,min(800,x_dist_step*4)))
     x_mesh, y_mesh = np.meshgrid(x_arr, y_arr)
 
+    def nullify_outside(z,x,y):
+        for col in range(x.shape[1]):
+            for row in range(y.shape[0]):
+                if not isLegalPoint(y[row,col],x[row,col]):
+                    z[row,col] = np.nan
+
     # Grid the values
     z_mesh = griddata((y_orig, x_orig), z_orig, (x_mesh, y_mesh), method='linear')
+
+    nullify_outside(z_mesh,x_mesh,y_mesh)
+
+    non_zero_count1 = np.sum(~np.isnan(z_mesh))
+    # Gaussian filter the grid to make it smoother
+    if smooth_sigma_km>0:
+        sigma = km_to_degree(smooth_sigma_km,smooth_sigma_km)
+        sigma[1] = sigma[1]/ x_step_dist
+        sigma[0] = sigma[0]/ y_step_dist
+        z_mesh = gaussian_filter(z_mesh, sigma, mode='constant')
+        nullify_outside(z_mesh, x_mesh, y_mesh)
 
     if threshold!=None:
         z_mesh[z_mesh>threshold]=np.nan
 
-    # Gaussian filter the grid to make it smoother
-    #sigma = [2,2]
-    #z_mesh = sp.ndimage.filters.gaussian_filter(z_mesh, sigma, mode='constant')
+    non_zero_count2 = np.sum(~np.isnan(z_mesh))
+    print("... non-nan counts %i --> %i" % (non_zero_count1,non_zero_count2))
 
     # Create the contour
     levels = len(cm.colors)
@@ -341,8 +397,9 @@ if __name__ == "__main__":
     results = []
     if os.path.isfile(RESULTFILE):
         old_results, old_points, old_TRANSPORT_PARAMETERS = pickle.load(open(RESULTFILE, "rb"))
-        if old_TRANSPORT_PARAMETERS==TRANSPORT_PARAMETERS and points==old_points:
+        if (old_TRANSPORT_PARAMETERS==TRANSPORT_PARAMETERS and points==old_points) or USE_OLD_DATA:
             results = old_results
+            points =  old_points
             print("old data loaded, already have %i of %i points" % (len(results),len(points)))
 
     # start polling
@@ -395,37 +452,43 @@ if __name__ == "__main__":
     # get global color maps for contours (first) and points (two latter ones)
     cm,color_list,TIME_RANGES_dense = get_colormap(z)
 
-    #flatten_list = lambda l: [item for sublist in l for item in sublist]
-    # Initialize map
-    my_map_global = folium.Map(location=TARGET_COORDINATE, zoom_start=10)
+    # create one map for each smoothing factor
+    for smooth_km in [0,0.05,0.1,0.5]:
 
-    print("drawing layers")
-    # add combined contour
-    print("..full contour")
-    my_map_global = add_contour(x, y, z, my_map_global,cm,"Full_contour")
+        #flatten_list = lambda l: [item for sublist in l for item in sublist]
+        # Initialize map
+        my_map_global = folium.Map(location=TARGET_COORDINATE, zoom_start=10)
 
-    # plot individual time layers
-    for i in range(len(TIME_RANGES)):
-        #color = colors.to_hex(color_list[i],keep_alpha=False)
-        s = "Max_%imin" % (TIME_RANGES[i])
-        print("..layer %s" % s)
-        #create_convexhull_polygon(my_map_global,pointlist[i],layer_name=s,line_color=None, fill_color=color, weight=5,text=s)
-        my_map_global = add_contour(x, y, z, my_map_global,cm,s,threshold=TIME_RANGES[i],add_colormap=False,show=False)
+        print("drawing layers")
+        # add combined contour
+        print("..full contour")
+        my_map_global = add_contour(x, y, z, my_map_global,cm,"Full_contour",smooth_sigma_km=smooth_km)
 
-    # plot point layers
-    print("drawing points")
-    print("..layer poll points")
-    draw_points(my_map_global,points, layer_name='Poll_points',color='royalblue',show=False)
-    print("..layer target")
-    draw_points(my_map_global,[TARGET_COORDINATE], layer_name='Target',color=['red'],text=["Target %s" % str(TARGET_COORDINATE)],radius=1)
-    print("..layer route points")
-    draw_points(my_map_global, [points[i] for i in ind_tip], layer_name='Points_with_route',
-                color=[color_list[get_color_index(z[i], TIME_RANGES_dense)] for i in ind_tip], text=info, show=False,
-                radius=20)
+        # plot individual time layers
+        for i in range(len(TIME_RANGES)):
+            #color = colors.to_hex(color_list[i],keep_alpha=False)
+            s = "Max_%imin" % (TIME_RANGES[i])
+            print("..layer %s" % s)
+            #create_convexhull_polygon(my_map_global,pointlist[i],layer_name=s,line_color=None, fill_color=color, weight=5,text=s)
+            my_map_global = add_contour(x, y, z, my_map_global,cm,s,threshold=TIME_RANGES[i],add_colormap=False,show=False)
 
-    folium.LayerControl(collapsed=False).add_to(my_map_global)
+        # plot point layers
+        print("drawing points")
+        print("..layer poll points")
+        draw_points(my_map_global,points, layer_name='Poll_points',color='royalblue',show=False)
+        print("..layer target")
+        draw_points(my_map_global,[TARGET_COORDINATE], layer_name='Target',color=['red'],text=["Target %s" % str(TARGET_COORDINATE)],radius=1)
+        print("..layer route points")
+        draw_points(my_map_global, [points[i] for i in ind_tip], layer_name='Points_with_route',
+                    color=[color_list[get_color_index(z[i], TIME_RANGES_dense)] for i in ind_tip], text=info, show=False,
+                    radius=20)
 
-    print("saving the map")
-    my_map_global.save("mapper_results.html")
+        folium.LayerControl(collapsed=False).add_to(my_map_global)
+
+        print("saving the map")
+        if smooth_km>0:
+            my_map_global.save("mapper_results_smoothed_%0.3fkm.html" % smooth_km)
+        else:
+            my_map_global.save("mapper_results.html")
 
     print("All done!")
